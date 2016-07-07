e05ac670aeb51255c9407ef1c6c92edf89ec04dc
natuchasca <natuxalee@gmail.com>
2016-07-07 13:18:21 -0400

    trabajado js...

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-$(document).ready(function()) {
-	// var jugador; var turno; var alertas = $(".alertas");
+// 2 jugadores. Cada caja tiene 2 valores X y O
 
+$(document).ready(function() {
+	var jugadorX = "X";
+	var jugadorO = "O"; 
+	var jugando = jugadorX;
+	//var turno; var alertas = $(".alertas");
 
-	$(".box").click(function()) {
-		box = $(this);
+	$(".box").click(function() {
+		this.innerHTML = "X";
+		$(".box")
+	});
 
-	}
+	var turno = function() {
+	    if (jugando === jugadorX) {
+	      jugando = jugadorO;
+	    } else {
+	      jugando = jugadorX;
+	    }
+	  };
 
 	
-}
+
+	/*;box.addEventListener("click",
+	function () {
+	    box.innerHTML = "X";
+	});
+$(".btn").click(function() {
+		box.innerHTML = "";
+	} 
+
+*/
+	
+	
+	
+});
